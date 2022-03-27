2dadcfc7520db8a265b6a0ea67528c2027e5d14c
Kosisochukwu <emmanuelobidigwe63@gmail.com>
2022-03-27 07:39:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,8 +4,59 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+//Scroll reveal animations
+//Common reveal options to create reveal animations
 
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 100
 });
+    
+//Target elements, and specify options to create reveal animations
+ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay: 500,
+origin: 'left' });
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 600, origin: 
+'right' });
+ScrollReveal().reveal('.home .info .btn', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200 });
+ScrollReveal().reveal('.home-img, .about-img', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.about .description, .contact-right', { delay: 600, origin: 'right' });
+ScrollReveal().reveal('.about .professional-list li', { delay: 500, origin: 'right', interval:
+200 });
+ScrollReveal().reveal('.skills-description, services-description, .contact-card, .client-swiper, contact-left h2', { delay: 700, origin: 'left', });
+ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 800, origin: 'bottom', interval: 200 });
+ScrollReveal().reveal('footer .group', { delay: 500, origin: 'top', interval: 200 });
+
+
+
+
+
+
+//Services section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
+
 
 
 //Portfolio section - Modal
@@ -49,7 +100,6 @@ var swiper = new Swiper(".client-swiper", {
     },
 });
 
-
 //Scroll to Top button
 const scrollTopBtn = document.querySelector(".scrollToTop-btn");
 
@@ -61,36 +111,6 @@ scrollTopBtn.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 });
-
-
-
-//Responsive navigation menu toogle
-const menuBtn = document.querySelector(".nav-menu-btn");
-const closeBtn = document.querySelector(".nav-close-btn");
-const navigation = document.querySelector(".navigation");
-const navItems = document.querySelector(".nav-items a");
-
-menuBtn.addEventListener("click", () => {
-    navigation.classList.add("active");
-});
-
-closeBtn.addEventListener("click", () => {
-    navigation.classList.remove("active");
-});
-
-navItems.forEach((navItem) =>{
-    navItem.addEventListener("click", () => {
-        navigation.classList.remove("active");
-    });
-});
-
-
-
-
-
-
-
-
 
 
 //Navigation menu items active on page scroll
@@ -114,34 +134,6 @@ window.addEventListener("scroll", () => {
 });
 
 
-
-
-
-
-//Scroll reveal animations
-//Common reveal options to create reveal animations
-ScrollReveal({
-    //reset: true,
-    distance: '60px',
-    duration: 2500,
-    delay: 100
-});
-    
-//Target elements, and specify options to create reveal animations
-ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay: 500,
-origin: 'left' });
-ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 600, origin: 
-'right' });
-ScrollReveal().reveal('.home .info .btn', { delay: 700, origin: 'bottom' });
-ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200 });
-ScrollReveal().reveal('.home-img, .about-img', { delay: 500, origin: 'bottom' });
-ScrollReveal().reveal('.about .description, .contact-right', { delay: 600, origin: 'right' });
-ScrollReveal().reveal('.about .professional-list li', { delay: 500, origin: 'right', interval:
-200 });
-ScrollReveal().reveal('.skills-description, services-description, .contact-card, .client-swiper, contact-left h2', { delay: 700, origin: 'left', });
-ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 800, origin: 'bottom', interval: 200 });
-ScrollReveal().reveal('footer .group', { delay: 500, origin: 'top', interval: 200 });
-
 //Website dark/light theme
 const themeBtn = document.querySelector(".theme-btn");
 
@@ -162,29 +154,24 @@ const savedIcon = localStorage.getItem("saved-icon");
 if (savedTheme){
     document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
     themeBtn.classList[savedIcon === "sun" ? "add" : "remove"](sun);
-}
+};
 
+//Responsive navigation menu toogle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
 
-
-//Services section - Modal
-const serviceModals = document.querySelectorAll(".service-modal");
-const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
-const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
-
-var modal = function(modalClick){
-    serviceModals[modalClick].classList.add("active");
-}
-
-learnmoreBtns.forEach((learnmoreBtn, i) => {
-    learnmoreBtn.addEventListener("click", () => {
-        modal(i);
-    });
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
 });
 
-modalCloseBtns.forEach((modalCloseBtn) => {
-    modalCloseBtn.addEventListener("click", () => {
-        serviceModals.forEach((modalView) => {
-            modalView.classList.remove("active");
-        });
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) =>{
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
     });
+});
 
