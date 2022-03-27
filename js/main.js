3fe237c027c069e7d10e8873f7394951dc51d578
Kosisochukwu <emmanuelobidigwe63@gmail.com>
2022-03-27 07:29:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -109,27 +109,7 @@ scrollTopBtn.addEventListener("click", () => {
 
 
 
-//Website dark/light theme
-const themeBtn = document.querySelector(".theme-btn");
 
-themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-theme");
-    themeBtn.classList.toggle("sun");
-
-    localStorage.setItem("saved-theme", getCurrentTheme());
-    localStorage.setItem("saved-icon", getCurrentIcon());
-});
-
-const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
-const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
-
-const savedTheme = localStorage.getItem("saved-theme");
-const savedIcon = localStorage.getItem("saved-icon");
-
-if (savedTheme){
-    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
-    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"](sun);
-}
 
 //Navigation menu items active on page scroll
 window.addEventListener("scroll", () => {
@@ -180,7 +160,27 @@ ScrollReveal().reveal('.skills-description, services-description, .contact-card,
 ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 800, origin: 'bottom', interval: 200 });
 ScrollReveal().reveal('footer .group', { delay: 500, origin: 'top', interval: 200 });
 
+//Website dark/light theme
+const themeBtn = document.querySelector(".theme-btn");
 
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+
+    localStorage.setItem("saved-theme", getCurrentTheme());
+    localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if (savedTheme){
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"](sun);
+}
 
 
 
